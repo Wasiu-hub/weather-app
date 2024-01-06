586eb311e16d40eeedeb222e14181c957bdf033b
Wasiu-hub <contactbow@yahoo.com>
2024-01-06 23:27:58 +0000

    state created to render selected date

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 /* eslint-disable react/react-in-jsx-scope */
 import "../styles/App.css";
-import React from "react";
+import React, { useState } from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
 function App({ location, forecasts }) {
   const { city, country } = location;
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
   return (
     <div className="weather-app">
       {/* <h1>Weather App</h1> */}
       <LocationDetails city={city} country={country} />
       <ForecastSummaries forecasts={forecasts} />
-      <ForecastDetails forecast={forecasts[0]} />
+      <ForecastDetails forecast={selectedForecast} />
     </div>
   );
 }
