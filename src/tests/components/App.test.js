2730597d8a7894367752d35605049e7b082a6fad
Wasiu-hub <contactbow@yahoo.com>
2024-01-25 20:55:34 +0000

    all tests updated but App.test failed

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,22 +1,13 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/react-in-jsx-scope */
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import App from "../../components/App";
-import forecast from "../../data/forecast.json";
 
 describe("App", () => {
-  // NOTE: WHEN THIS IS NOT COMMENTED OUT (for weather App in App.js)
-  // test("renders learn react link", () => {
-  //   // eslint-disable-next-line react/react-in-jsx-scope
-  //   // render(<App location={{}} />);
-  //   // render(<App location />);
-  //   const linkElement = screen.getByText(/Weather App/i);
-  //   expect(linkElement).toBeInTheDocument();
-  // });
-
-  it("renders App component correctly", () => {
-    render(<App location={forecast.location} forecasts={forecast.forecasts} />);
-    const h1Element = screen.getByText(/Manchester, UK/i);
-    expect(h1Element).toBeInTheDocument();
+  it("renders correctly", () => {
+    render(<App />);
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
