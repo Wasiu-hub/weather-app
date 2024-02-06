56064004af8dc4815fb584a4fd831d1631e27418
Wasiu-hub <contactbow@yahoo.com>
2024-02-06 21:08:44 +0000

    test for search componenet updated and passes

--- src/tests/components/SearchForm.test.js
+++ src/tests/components/SearchForm.test.js
@@ -19,4 +19,15 @@ describe("SearchForm", () => {
     );
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it("renders the correct text in the button element", () => {
+    const { getByText } = render(
+      <SearchForm
+        searchText="Manchester"
+        setSearchText={validProps.setSearchText}
+        onSubmit={validProps.handleCitySearch}
+      />
+    );
+    expect(getByText("Search")).toBeInstanceOf(HTMLButtonElement);
+  });
 });
